c68d318ff943129570b42fefd24fbdf1e5cad54b
Alexandro Luongo <W00dL3cs@users.noreply.github.com>
2016-11-30 20:19:34 +0100

    Initial commit. "Dumper" code refactoring.

---
+++ Dumper/Program.cs
@@ -0,0 +1,117 @@
+﻿//
+//  File Name: Program.cs
+//  Project Name: Dumper
+//
+//  Created by Alexandro Luongo on 30/11/2016.
+//  Copyright © 2016 Alexandro Luongo. All rights reserved.
+//
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Dumper
+{
+    class Program
+    {
+        private static readonly string DIRECTORY = "Files";
+        private static readonly string DESTINATION = "Keys.txt";
+
+        private static List<string> Keys;
+
+        private static void Init()
+        {
+            Console.WriteLine("PwnFast - PokeFast Cracking Utility");
+            Console.WriteLine("Copyright (C) 2016 - W00dL3cs (Alexandro Luongo)");
+            Console.WriteLine();
+
+            Keys = new List<string>();
+
+            Directory.CreateDirectory(DIRECTORY);
+        }
+
+        static void Main(string[] args)
+        {
+            Init();
+
+            DumpKeys();
+
+            StayOpen();
+        }
+
+        private static void StayOpen()
+        {
+            while (true) ;
+        }
+
+        private static async void DumpKeys()
+        {
+            Console.WriteLine(string.Format("Loading APKs inside \"{0}\" folder...", DIRECTORY));
+
+            var APKs = Directory.GetFiles(DIRECTORY, "*.apk", SearchOption.TopDirectoryOnly);
+
+            Console.WriteLine(string.Format("Successfully found {0} APKs!", APKs.Length));
+            Console.WriteLine();
+
+            var Tasks = new List<Task>();
+
+            foreach (var APK in APKs)
+            {
+                Tasks.Add(DumpKey(APK));
+            }
+
+            await Task.WhenAll(Tasks);
+
+            File.WriteAllLines(DESTINATION, Keys.ToArray());
+
+            Console.WriteLine();
+            Console.WriteLine(string.Format("A total of {0} keys has been written to the file \"{1}\".", Keys.Count, DESTINATION));
+            Console.WriteLine();
+
+            Console.WriteLine("Press a key to terminate.");
+            Console.ReadKey();
+
+            Environment.Exit(0);
+        }
+
+        private static async Task DumpKey(string APK)
+        {
+            try
+            {
+                Console.WriteLine("Reading {0}...", APK);
+
+                using (var Archive = ZipFile.OpenRead(APK))
+                {
+                    var Resources = Archive.GetEntry("resources.arsc");
+
+                    if (Resources != null)
+                    {
+                        using (var Reader = new StreamReader(Resources.Open()))
+                        {
+                            var Content = await Reader.ReadToEndAsync();
+
+                            var Regex = new Regex("([0-9A-Za-z]{8}[-][0-9A-Za-z]{4}[-][0-9A-Za-z]{4}[-][0-9A-Za-z]{4}[-][0-9A-Za-z]{12})", RegexOptions.Multiline);
+
+                            foreach (Match Match in Regex.Matches(Content))
+                            {
+                                var Key = Match.Groups[1].Value;
+
+                                Console.WriteLine(string.Format("API key found: {0}!", Key));
+
+                                Keys.Add(Key);
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(string.Format("Fatal exception while reading APK: {0}!", APK));
+                Console.WriteLine(string.Format("Error message: {0}", e.Message));
+            }
+        }
+    }
+}
